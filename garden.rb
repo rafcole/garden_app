ecbc7bf9910fc394ed6f9aa264d24dfdfc465812
Richard Cole <richard.af.cole@gmail.com>
2022-05-23 10:07:16 -0700

    sign in route tests complete

--- garden.rb
+++ garden.rb
@@ -1,9 +1,12 @@
 require "yaml"
+require 'date'
 
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 require "bcrypt"
+
+require_relative 'custom_classes/garden_class'
 
 # session needs in app?
 configure do
@@ -11,16 +14,26 @@ configure do
   set :session_secret, 'super secret'
 end
 
-# users route
-def user_path
+# this is where the id_209384.yml files for inidividual users will live
+def user_data_path
   if ENV["RACK_ENV"] == "test"
-    File.expand_path("../test/users", __FILE__)
+    File.expand_path("../test/user_data", __FILE__)
+    # all_users.yml
+    # id_23423.yml
+    # id_87837.yml ...
   else
-    File.expand_path("../users", __FILE__)
+    File.expand_path("../user_data", __FILE__)
   end
 end
 
-# test route
+def load_user_credentials
+  credentials_path = if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/user_data/all_users.yml", __FILE__)
+  else
+    File.expand_path("../user_datea/all_users.yml", __FILE__)
+  end
+  YAML.load_file(credentials_path)
+end
 
 # homepage
 get "/" do
@@ -29,12 +42,39 @@ end
 
 # sign in
 get "/signin" do 
+  if session[:user]
+    session[:message] = "You are currently signed in as #{session[:user]}. <a href=/signout> Click here to signout</a>"
+    redirect "/"
+  end
+
   erb :signin
+end
+
+def valid_credentials?(user_name, password)
+  user_hash = load_user_credentials
+
+  if user_hash[user_name]
+    password_from_file = BCrypt::Password.new(user_hash[user_name])
+    password_from_file == password
+  else
+    false
+  end
 end
 
 # sign in post
 post "/signin" do
-  redirect "/"
+  user_name = params["username"]
+  password = params["password"]
+  # if the password is correct, allow log in
+  if valid_credentials?(user_name, password)
+    session[:user] = user_name
+    session[:message] = "Welcome #{params["username"]}"
+    redirect "/"
+  else
+    status 422
+    session[:message] = "log in failed - generic"
+    erb :signin
+  end
 end
 
 # sign up
@@ -44,10 +84,19 @@ end
 
 # submit sign up
 post "/signup" do
-  redirect "/"
+
 end
 
-post "/addgarden" do
-  redirect "/"
+# add/remove garden page
+get "/add-garden" do
 end
 
+
+get "/edit-garden" do
+  erb :edit_gardens
+end
+
+# submit data to add/remove garden
+post "/edit-garden" do
+  erb :edit_gardens
+end
--- test/garden_test.rb
+++ test/garden_test.rb
@@ -6,6 +6,7 @@ require "minitest/autorun"
 require "rack/test"
 
 require_relative "../garden"
+#require_relative "../garden_class"
 
 class GardenTest < Minitest::Test
 
@@ -21,10 +22,15 @@ class GardenTest < Minitest::Test
   # logged in user session - was admin session
 
   def setup
+    FileUtils.mkdir_p(user_data_path)
   end
 
   def teardown
     # delete contents of /test/users
+  end
+
+  def session
+    last_request.env["rack.session"]
   end
 
   def test_homepage_keystones_signed_out
@@ -37,19 +43,48 @@ class GardenTest < Minitest::Test
     assert_includes last_response.body, "href=/signin"
     assert_includes last_response.body, "href=/signup"
 
-    # check for sign up link
-
     # check for main header areas
-      # Title area
-      assert_includes last_response.body, "<h1>Your garden</h1>"
-
-      # add garden area (should be a link, notify to log in to toggle between static message and link)
-      assert_includes last_response.body, "Log in to add a gardening area"
-      # Garden areas as separate H1s?
-        # loren impsum examples?
-      # Summary at the bottom
-      assert_includes last_response.body, "See all of your garden needs"
-      # is this yield_content?
-    
+    # Title area
+    assert_includes last_response.body, "Your Gardens"
+    # add garden area (should be a link, notify to log in to toggle between static message and link)
+    assert_includes last_response.body, %q(href=/add-garden)
   end
+
+  def test_sign_in_no_user
+    get "/signin"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<button type='submit')
+  end
+
+
+  def test_log_in_valid_credentials    
+    post "/signin", { username: 'admin', password: 'secret' }
+    assert_equal "Welcome admin", session[:message] 
+    assert_equal 'admin', session[:user]
+    assert_equal 302, last_response.status
+  end
+
+  def test_log_in_invalid_password
+    post "/signin", { username: 'admin', password: 'wrong' }
+    assert_includes last_response.body, "log in failed - generic"
+    assert_nil session[:user]
+    assert_equal 422, last_response.status
+  end
+
+  def test_log_in_invalid_username
+    post "/signin", { username: 'wrong', password: 'wrong' }
+    assert_includes last_response.body, "log in failed - generic"
+    assert_nil session[:user]
+    assert_equal 422, last_response.status
+  end
+
+
+
+  # def test_garden_setter_methods
+  #   foo = Harvest.new("brocolli")
+
+  #   assert_raises(ArgumentError) { foo.num_plants = ("bar") }
+  # end
 end
